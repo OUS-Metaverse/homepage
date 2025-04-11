9e4c9bc2dec4521c89b90a0d18cfcef8928763e6
Minetake <minetake01.m@gmail.com>
2025-04-11 17:31:50 +0900

    feat: ヘッダーとフッターを実装

--- src/libs/i18n.ts
+++ src/libs/i18n.ts
@@ -23,3 +23,16 @@ export function useTranslations(url: URL) {
         allRoutes: getAllLocaleUrls(url),
     };
 }
+
+export function getNameFromLocale(locale: string) {
+    switch (locale) {
+        case "ja":
+            return "日本語";
+        case "en":
+            return "English";
+        case "zh":
+            return "中文";
+        default:
+            throw new Error(`Locale ${locale} not supported`);
+    }
+}
